c73e607b9a8adaf9e6f1802e1245e7a984b312e6
KONY <lhy159840@outlook.com>
2021-04-25 16:00:32 +0800

    Update workflowruntime_types.go
    The comment in workFlowRuntime Number filed has grammatical errors

--- api/v1alpha1/workflowruntime_types.go
+++ api/v1alpha1/workflowruntime_types.go
@@ -78,7 +78,7 @@ type ProcessRuntimes map[string]ProcessRuntime
 
 // ProcessRuntime records the process runtime info
 type ProcessRuntime struct {
-	// Number is the number the processes run the same Function
+	// Number is the number of the processes running the same Function
 	Number int `json:"int"`
 	// TODO: Add more fileds
 }
